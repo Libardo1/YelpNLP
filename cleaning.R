6ecf252a6f2c3c247568e2eeb8b369f640828577
victorkong94 <victorkong@berkeley.edu>
2015-08-03 10:38:58 -0700

    Clean data

--- cleaning.R
+++ cleaning.R
@@ -2,43 +2,32 @@
 library(RJSONIO, quietly = TRUE)
 
 # Vector of levels for response
-starsF = c("low", "mid", "high")
+levels = c("low", "mid", "high")
 
 # Extract business IDs of restaurants only
 temp = as.data.frame(t(sapply(readLines("business.json"), fromJSON)))
-restaurant = logical(nrow(temp))
-for (i in 1:nrow(temp)) {
-  restaurant[i] = "Restaurants" %in% temp[,5][[i]]
-}
+restaurant = sapply(temp[,5], function(x) "Restaurants" %in% x)
 businessID = as.character(temp$business_id[restaurant])
 
 # Extract rating and review data from challenge dataset
-temp = data.frame(fromJSON(readLines("...", n = 1)), stringsAsFactors = FALSE)
-temp = temp[seq(3, nrow(temp), 3),]
-temp = temp[temp$business_id %in% businessID,]
-train = data.frame(
-  stars = numeric(nrow(temp)),
-  starsF = NA,
-  text = NA
-)
-for (i in 1:nrow(temp)/3) {
-  training$stars[i] = temp[,4][3 * i]
-  training$text[i] = temp[,6][3 * i]
+temp = matrix(sapply(readLines("train.json"), function(x) unlist(fromJSON(x))),
+              ncol = 10, byrow = TRUE)
+temp = temp[temp[,10] %in% businessID,]
+train = data.frame(stars = numeric(nrow(temp)), starsF = NA, text = NA)
+for (i in 1:nrow(temp)) {
+  train$stars[i] = as.numeric(temp[i,6])
+  train$text[i] = temp[i,8]
 }
-training$starsF = cut(training$stars, seq(.5, 5.5, 1.66), labels = starsF)
-save(training, "train.rda")
+train$starsF = cut(train$stars, seq(.5, 5.5, 1.66), labels = levels)
+save(train, file = "train.rda")
 
 # Extract rating and review data from academic dataset
-temp = data.frame(fromJSON(readLines("...", n = 1)), stringsAsFactors = FALSE)
-temp = temp[seq(3, nrow(temp), 3),]
-test = data.frame(
-  stars = numeric(nrow(temp)/3),
-  starsF = NA,
-  text = NA
-)
-for (i in 1:nrow(temp)/3) {
-  test$stars[i] = temp[,6][3 * i]
-  test$text[i] = temp[,5][3 * i]
+temp = matrix(sapply(readLines("test.json"), function(x) unlist(fromJSON(x))),
+              ncol = 10, byrow = TRUE)
+test = data.frame(stars = numeric(nrow(temp)), starsF = NA, text = NA)
+for (i in 1:nrow(temp)) {
+  test$stars[i] = as.numeric(temp[i,8])
+  test$text[i] = temp[i,7]
 }
-test$starsF = cut(test$stars, seq(.5, 5.5, 1.66), labels = starsF)
-save(test, "test.rda")
+test$starsF = cut(test$stars, seq(.5, 5.5, 1.66), labels = levels)
+save(test, file = "test.rda")
