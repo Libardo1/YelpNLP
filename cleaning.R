bb3d787c275f6d2456d914814d974e702706b286
victorkong94 <victorkong@berkeley.edu>
2015-08-03 22:53:50 -0700

    Clean data

--- cleaning.R
+++ cleaning.R
@@ -17,7 +17,6 @@ train = data.frame(stars = numeric(nrow(temp)), starsF = NA, text = NA)
 train$stars = as.numeric(temp[,6])
 train$starsF = cut(train$stars, c(.5, 3.5, 5.5), labels = levels)
 train$text = temp[,8]
-save(train, file = "train.rda")
 
 # Extract rating and review data from academic dataset
 temp = matrix(sapply(readLines("test.json"), function(x) unlist(fromJSON(x))),
@@ -26,4 +25,30 @@ test = data.frame(stars = numeric(nrow(temp)), starsF = NA, text = NA)
 test$stars = as.numeric(temp[,8])
 test$starsF = cut(test$stars, c(.5, 3.5, 5.5), labels = levels)
 test$text = temp[,7]
+
+# Remove line breaks
+train$text = gsub("\n", "", train$text)
+test$text = gsub("\n", "", test$text)
+
+# Remove quotations and backslashing out of quotations
+train$text = gsub('\"', "", train$text)
+train$text = gsub('"', "", train$text)
+train$text = gsub("'", "", train$text)
+test$text = gsub('\"', "", test$text)
+test$text = gsub('"', "", test$text)
+test$text = gsub("'", "", test$text)
+
+# Remove "c" with cedilla
+train$text = gsub("\xe7", "c", train$text)
+test$text = gsub("\xe7", "c", test$text)
+
+# Remove "e" with acute accent
+train$text = gsub("\xe9", "e", train$text)
+test$text = gsub("\xe9", "e", test$text)
+
+# Remove registered trademark symbol
+train$text = gsub("\xae", "", train$text)
+test$text = gsub("\xae", "", test$text)
+
+save(train, file = "train.rda")
 save(test, file = "test.rda")
