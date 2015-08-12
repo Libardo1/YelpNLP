cff66c2e2734e6ac469cf81b8fd4125acaf8bbdd
victorkong94 <victorkong@berkeley.edu>
2015-08-12 01:01:57 -0700

    Resolve all previous issues

--- cleaning.R
+++ cleaning.R
@@ -4,97 +4,81 @@ library(RJSONIO, quietly = TRUE)
 # Vector of levels for response
 levels = c("low", "high")
 
-# Extract business IDs of restaurants in contiguous US only
+# Extract business IDs of restaurants west of Montreal only
 temp = as.data.frame(t(sapply(readLines("business.json"), fromJSON)))
-temp = temp[temp[,10] > -124.785 & temp[,10] < -66.947028 &
-              temp[,13] > 24.446667 & temp[,13] < 49.384472,]
+temp = temp[temp[,10] < -75,]
 temp = temp[sapply(temp[,5], function(x) "Restaurants" %in% x),]
 businessID = as.character(temp[,1])
 
 # Extract rating and review data from challenge dataset
-temp = matrix(sapply(readLines("train.json"), function(x) unlist(fromJSON(x))),
+temp = matrix(sapply(readLines("review.json"), function(x) unlist(fromJSON(x))),
               ncol = 10, byrow = TRUE)
 temp = temp[temp[,10] %in% businessID,]
-train = data.frame(stars = numeric(nrow(temp)), starsF = NA, text = NA)
-train$stars = as.numeric(temp[,6])
-train$starsF = cut(train$stars, c(.5, 3.5, 5.5), labels = levels)
-train$text = temp[,8]
-
-# Extract rating and review data from academic dataset
-temp = matrix(sapply(readLines("test.json"), function(x) unlist(fromJSON(x))),
-              ncol = 10, byrow = TRUE)
-test = data.frame(stars = numeric(nrow(temp)), starsF = NA, text = NA)
-test$stars = as.numeric(temp[,8])
-test$starsF = cut(test$stars, c(.5, 3.5, 5.5), labels = levels)
-test$text = temp[,7]
+data = data.frame(stars = numeric(nrow(temp)), starsF = NA, text = NA)
+data$stars = as.numeric(temp[,6])
+data$starsF = cut(data$stars, c(.5, 3.5, 5.5), labels = levels)
+data$text = temp[,8]
 
 # Remove line breaks
-train$text = gsub("\n", "", train$text)
-train$text = gsub("\r", "", train$text)
-test$text = gsub("\n", "", test$text)
-test$text = gsub("\r", "", test$text)
+data$text = gsub("\n", "", data$text)
+data$text = gsub("\r", "", data$text)
 
 # Remove quotations and backslashing out of quotations
-train$text = gsub('\"', "", train$text)
-train$text = gsub('"', "", train$text)
-train$text = gsub("'", "", train$text)
-test$text = gsub('\"', "", test$text)
-test$text = gsub('"', "", test$text)
-test$text = gsub("'", "", test$text)
+data$text = gsub('\"', "", data$text)
+data$text = gsub('"', "", data$text)
+data$text = gsub("'", "", data$text)
 
 # Remove common unsupported characters
-train$text = gsub("\x86", "", train$text)
-train$text = gsub("\x92", "", train$text)
-train$text = gsub("\x94", "", train$text)
-train$text = gsub("\x99", "", train$text)
-train$text = gsub("\xa0", "", train$text)
-train$text = gsub("\xa1", "!", train$text)
-train$text = gsub("\xa2", " cents ", train$text)
-train$text = gsub("\xac", "", train$text)
-train$text = gsub("\xae", "", train$text)
-train$text = gsub("\xb0", " degrees ", train$text)
-train$text = gsub("\xb4", "", train$text)
-train$text = gsub("\xb6", "", train$text)
-train$text = gsub("\xbc", "", train$text)
-train$text = gsub("\xbe", "", train$text)
-train$text = gsub("\xc0", "A", train$text)
-train$text = gsub("\xc1", "A", train$text)
-train$text = gsub("\xc9", "E", train$text)
-train$text = gsub("\xd7", "", train$text)
-train$text = gsub("\xdc", "U", train$text)
-train$text = gsub("\xe0", "a", train$text)
-train$text = gsub("\xe1", "a", train$text)
-train$text = gsub("\xe3", "a", train$text)
-train$text = gsub("\xe7", "c", train$text)
-train$text = gsub("\xe8", "e", train$text)
-train$text = gsub("\xe9", "e", train$text)
-train$text = gsub("\xea", "e", train$text)
-train$text = gsub("\xeb", "e", train$text)
-train$text = gsub("\xec", "i", train$text)
-train$text = gsub("\xed", "i", train$text)
-train$text = gsub("\xef", "i", train$text)
-train$text = gsub("\xf1", "n", train$text)
-train$text = gsub("\xf2", "o", train$text)
-train$text = gsub("\xf3", "o", train$text)
-train$text = gsub("\xf4", "o", train$text)
-train$text = gsub("\xf6", "o", train$text)
-train$text = gsub("\xfa", "u", train$text)
-train$text = gsub("\xfb", "u", train$text)
-train$text = gsub("\xfc", "u", train$text)
-train$text = gsub("(]\x8coJDWKc_)", "", train$text)
-train$text = gsub("8\x8cF", "", train$text)
-train$text = gsub("4n|G", "", train$text)
-train$text = gsub("\005\x92\177\xdc\002", "", train$text)
+data$text = gsub("\x86", "", data$text)
+data$text = gsub("\x92", "", data$text)
+data$text = gsub("\x94", "", data$text)
+data$text = gsub("\x99", "", data$text)
+data$text = gsub("\xa0", "", data$text)
+data$text = gsub("\xa1", "!", data$text)
+data$text = gsub("\xa2", " cents ", data$text)
+data$text = gsub("\xac", "", data$text)
+data$text = gsub("\xae", "", data$text)
+data$text = gsub("\xb0", " degrees ", data$text)
+data$text = gsub("\xb4", "", data$text)
+data$text = gsub("\xb6", "", data$text)
+data$text = gsub("\xbc", "", data$text)
+data$text = gsub("\xbe", "", data$text)
+data$text = gsub("\xc0", "A", data$text)
+data$text = gsub("\xc1", "A", data$text)
+data$text = gsub("\xc9", "E", data$text)
+data$text = gsub("\xd7", "", data$text)
+data$text = gsub("\xdc", "U", data$text)
+data$text = gsub("\xe0", "a", data$text)
+data$text = gsub("\xe1", "a", data$text)
+data$text = gsub("\xe3", "a", data$text)
+data$text = gsub("\xe7", "c", data$text)
+data$text = gsub("\xe8", "e", data$text)
+data$text = gsub("\xe9", "e", data$text)
+data$text = gsub("\xea", "e", data$text)
+data$text = gsub("\xeb", "e", data$text)
+data$text = gsub("\xec", "i", data$text)
+data$text = gsub("\xed", "i", data$text)
+data$text = gsub("\xef", "i", data$text)
+data$text = gsub("\xf1", "n", data$text)
+data$text = gsub("\xf2", "o", data$text)
+data$text = gsub("\xf3", "o", data$text)
+data$text = gsub("\xf4", "o", data$text)
+data$text = gsub("\xf6", "o", data$text)
+data$text = gsub("\xfa", "u", data$text)
+data$text = gsub("\xfb", "u", data$text)
+data$text = gsub("\xfc", "u", data$text)
+data$text = gsub("(]\x8coJDWKc_)", "", data$text)
+data$text = gsub("8\x8cF", "", data$text)
+data$text = gsub("4n|G", "", data$text)
+data$text = gsub("\005\x92\177\xdc\002", "", data$text)
 
 # Remove most reviews in French, German, Italian, Spanish
 temp = c(" [Jj]e ", " [Nn][[:alpha:]]+ [[:alpha:]]* pas ", " [Cc]est ", " [Ii]l[s]? ", " [Ee]lle[s] ", " [Nn]ous ", " qui ", " [Ii]ch ", " [Dd]as ", " [Ii]st ", " nicht ", " und ", " [Ee]in ", " [Dd]er? ", " [Ss]ie ", "\xdf", " y ", " por ", " [Qq]ue ", " muy ", " [Ee]lla ", " bien[ .!]", " dans ", " l[eo]s? ", " esta? ", " ser ", " buena? ", " [Uu]n[aoe]? ", " [Ii]kke ", " og ", "\xd8", "\xe4", "\xee")
 for (i in 1:length(temp)) {
-  train = train[-grep(temp[i], train$text),]
+  data = data[-grep(temp[i], data$text),]
 }
 
-# Remove unhelpful individual reviews
-temp = -c(490870, 498999, 804560, 820239)
-train = train[temp,]
+# Remove an individual unhelpful observation
+data = data[-780072,]
 
-save(train, file = "train.rda")
-save(test, file = "test.rda")
+save(data, file = "data.rda")
--- varSelection.R
+++ varSelection.R
@@ -1,11 +1,11 @@
-load("data.rda")
+load("dataRev.rda")
 
 # Disable scientific notation
 options(scipen = 999)
 
 # Function to compute categorical variables' "preference" for high/low rating
 highLow = function(x) {
-  counts = table(data$rating, x)
+  counts = table(dataRev$rating, x)
   low = counts[1,2]/(counts[1,1] + counts[1,2])
   high = counts[2,2]/(counts[2,1] + counts[2,2])
   times = max(high, low) / min(high, low)
@@ -13,16 +13,16 @@ highLow = function(x) {
 }
 
 # Create vector of categorical variables by helpfulness in predicting rating
-temp = which(sapply(data, is.factor))[-1]
+temp = which(sapply(dataRev, is.factor))[-1]
 categorical = numeric(length(temp))
 for (i in 1:length(temp)) {
-  categorical[i] = highLow(data[,temp[i]])
+  categorical[i] = highLow(dataRev[,temp[i]])
 }
-names(categorical) = names(data)[temp]
+names(categorical) = names(dataRev)[temp]
 categorical = sort(categorical, decreasing = TRUE)
 save(categorical, file = "categorical.rda")
 
 # Drop categorical variables with less than 2 units of predicting power
 lessThan2 = categorical[categorical < 2]
-dataFin = data[,!(names(data) %in% names(lessThan2))]
+dataFin = dataRev[,!(names(dataRev) %in% names(lessThan2))]
 save(dataFin, file = "dataFin.rda")
